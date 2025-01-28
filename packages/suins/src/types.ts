3ca88882db00e33a7cb42ddf538bed949b806947
Tony Lee <chunglee2018@u.northwestern.edu>
2025-01-28 10:03:11 -0500

    organize interface and types

--- packages/suins/src/types.ts
+++ packages/suins/src/types.ts
@@ -3,60 +3,25 @@
 import type { SuiClient } from '@mysten/sui/client';
 import type { TransactionObjectArgument, TransactionObjectInput } from '@mysten/sui/transactions';
 
-export type Network = 'mainnet' | 'testnet' | 'custom';
+// Interfaces
+// -----------------
 
-export type VersionedPackageId = {
-	latest: string;
-	v1: string;
-	[key: string]: string;
-};
-
-export type Config = Record<'mainnet' | 'testnet', PackageInfo>;
-
-export type CoinConfigType = {
+export interface CoinConfig {
 	type: string;
 	feed: string;
-};
+}
 
-export type DiscountInfo = {
+export interface DiscountInfo {
 	discountNft: TransactionObjectInput;
 	type: string;
 	isFreeClaim?: boolean;
-};
+}
 
-export type BaseParams = {
-	years: number;
-	coinConfig: CoinConfigType;
-	coin?: TransactionObjectInput;
-	couponCode?: string;
-	discountInfo?: DiscountInfo;
-	maxAmount?: bigint;
-	priceInfoObjectId?: string | null;
-};
-
-export type RegistrationParams = BaseParams & {
-	domain: string;
-};
-
-export type RenewalParams = BaseParams & {
-	nft: TransactionObjectInput;
-};
-
-export type ReceiptParams = {
-	paymentIntent: TransactionObjectArgument;
-	priceAfterDiscount: TransactionObjectArgument;
-	coinConfig: CoinConfigType;
-	coin?: TransactionObjectInput;
-	maxAmount?: bigint;
-	priceInfoObjectId?: string | null;
-};
-
-export type PackageInfo = {
+export interface PackageInfo {
 	packageId: string;
 	packageIdV1: string;
 	packageIdPricing: string;
 	suins: string;
-	displayObject?: string;
 	discountsPackage: {
 		packageId: string;
 		discountHouseId: string;
@@ -77,39 +42,10 @@ export type PackageInfo = {
 	utils?: {
 		packageId: string;
 	};
-	coins: {
-		[key: string]: CoinConfigType;
-	};
-};
+	coins: Record<string, CoinConfig>;
+}
 
-// The config for the SuinsClient.
-export type SuinsClientConfig = {
-	client: SuiClient;
-	/**
-	 * The network to use. Defaults to mainnet.
-	 */
-	network?: Network;
-	/**
-	 * We can pass in custom PackageIds if we want this to
-	 * be functional on localnet, devnet, or any other deployment.
-	 */
-	config?: Config;
-};
-
-/**
- * The price list for SuiNS names.
- */
-export type SuinsPriceList = Map<[number, number], number>;
-
-/**
- * The coin type and discount for SuiNS names.
- */
-export type CoinTypeDiscount = Map<string, number>;
-
-/**
- * A NameRecord entry of SuiNS Names.
- */
-export type NameRecord = {
+export interface NameRecord {
 	name: string;
 	nftId: string;
 	targetAddress: string;
@@ -118,4 +54,54 @@ export type NameRecord = {
 	avatar?: string;
 	contentHash?: string;
 	walrusSiteId?: string;
+}
+
+// Types
+// -----------------
+
+export type Network = 'mainnet' | 'testnet' | 'custom';
+
+export type VersionedPackageId = {
+	latest: string;
+	v1: string;
+	[key: string]: string;
 };
+
+export type Config = Record<'mainnet' | 'testnet', PackageInfo>;
+
+export type BaseParams = {
+	years: number;
+	coinConfig: CoinConfig;
+	coin?: TransactionObjectInput;
+	couponCode?: string;
+	discountInfo?: DiscountInfo;
+	maxAmount?: bigint;
+	priceInfoObjectId?: string | null;
+};
+
+export type RegistrationParams = BaseParams & {
+	domain: string;
+};
+
+export type RenewalParams = BaseParams & {
+	nft: TransactionObjectInput;
+};
+
+export type ReceiptParams = {
+	paymentIntent: TransactionObjectArgument;
+	priceAfterDiscount: TransactionObjectArgument;
+	coinConfig: CoinConfig;
+	coin?: TransactionObjectInput;
+	maxAmount?: bigint;
+	priceInfoObjectId?: string | null;
+};
+
+export type SuinsClientConfig = {
+	client: SuiClient;
+	network?: Network;
+	config?: Config;
+};
+
+export type SuinsPriceList = Map<[number, number], number>;
+
+export type CoinTypeDiscount = Map<string, number>;
