2e443dc9f61163ea468d034d0e7ee7168f86eac4
Jordan Gensler <jordan@mystenlabs.com>
2025-02-04 12:09:56 -0600

    meta

--- packages/docs/app/layout.tsx
+++ packages/docs/app/layout.tsx
@@ -31,6 +31,13 @@ const inter = Inter({
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
+			<head>
+				<meta
+					name="google-site-verification"
+					content="T-2HWJAKh8s63o9KFxCFXg5MON_NGLJG76KJzr_Hp0A"
+				/>
+				<meta httpEquiv="Content-Language" content="en" />
+			</head>
 			<body className="flex flex-col min-h-screen">
 				<RootProvider>{children}</RootProvider>
 			</body>
