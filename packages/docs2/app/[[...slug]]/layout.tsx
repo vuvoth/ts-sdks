a3cc8e0d878867be10a1e100c55ddbe392622336
Jordan Gensler <jordan@mystenlabs.com>
2025-02-04 10:32:55 -0600

    Typedocs

--- packages/docs2/app/[[...slug]]/layout.tsx
+++ packages/docs2/app/[[...slug]]/layout.tsx
@@ -8,7 +8,43 @@ import { source } from '@/lib/source';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<DocsLayout {...baseOptions} tree={source.pageTree}>
+		<DocsLayout
+			{...baseOptions}
+			tree={source.pageTree}
+			sidebar={{
+				tabs: [
+					{
+						title: 'Sui SDK',
+						description: 'TypeScript interfaces for Sui',
+						url: '/typescript',
+					},
+					{
+						title: 'BCS',
+						description: 'Encoding and decoding Sui objects',
+						url: '/bcs',
+					},
+					{
+						title: 'Dapp Kit',
+						description: 'Build Sui dapps in React',
+						url: '/dapp-kit',
+					},
+					{
+						title: 'Kiosk',
+						description: 'Interact with on-chain commerce applications',
+						url: '/kiosk',
+					},
+					{
+						title: 'zkSend',
+						description: 'Send Sui with a link',
+						url: '/zksend',
+					},
+					{
+						title: 'API Reference',
+						url: '/typedoc/index.html',
+					},
+				],
+			}}
+		>
 			{children}
 		</DocsLayout>
 	);
