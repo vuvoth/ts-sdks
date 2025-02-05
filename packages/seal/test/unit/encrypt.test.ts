2e3b12996e0822d09238f09fed74145294a5f021
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2025-02-05 10:06:20 +0100

    Fix plain roundtrip test + clean up

--- packages/seal/test/unit/encrypt.test.ts
+++ packages/seal/test/unit/encrypt.test.ts
@@ -4,7 +4,7 @@
 import { fromHex, toHex } from '@mysten/bcs';
 import { describe, expect, it } from 'vitest';
 
-import { AesGcm256 } from '../../src/aes';
+import { AesGcm256, Plain } from '../../src/aes';
 import { G1Element, G2Element, Scalar } from '../../src/bls12381';
 import { encrypt } from '../../src/encrypt';
 import { BonehFranklinBLS12381Services, DST } from '../../src/ibe';
@@ -13,7 +13,7 @@ import { KeyStore } from '../../src/key-store';
 import { EncryptedObject } from '../../src/types';
 import { createFullId } from '../../src/utils';
 
-describe('sdk tests', () => {
+describe('Seal encryption tests', () => {
 	function generateKeyPair(): [Scalar, G2Element] {
 		const sk = Scalar.random();
 		const pk = G2Element.generator().multiply(sk);
@@ -24,7 +24,7 @@ describe('sdk tests', () => {
 		return G1Element.hashToCurve(id).multiply(sk);
 	}
 
-	it('test encryption format', async () => {
+	it('sanity checks for encryption format', async () => {
 		const [, pk1] = generateKeyPair();
 		const [, pk2] = generateKeyPair();
 		const [, pk3] = generateKeyPair();
@@ -72,7 +72,7 @@ describe('sdk tests', () => {
 		expect(parsed.threshold).toEqual(2);
 	});
 
-	it('encryption e2e', async () => {
+	it('test encryption round-trip with AesGcm-mode', async () => {
 		const [sk1, pk1] = generateKeyPair();
 		const [sk2, pk2] = generateKeyPair();
 		const [sk3, pk3] = generateKeyPair();
@@ -160,73 +160,79 @@ describe('sdk tests', () => {
 			parsed.encrypted_shares.BonehFranklinBLS12381.shares.slice(0, 1);
 		await expect(key_store_1.decrypt(modified_parsed_1)).rejects.toThrow();
 	});
-	//
-	// it("encryption e2e plain", async () => {
-	//   const [sk1, pk1] = generateKeyPair();
-	//   const [sk2, pk2] = generateKeyPair();
-	//   const [sk3, pk3] = generateKeyPair();
-	//
-	//   const encryption = await encrypt(
-	//     [
-	//       { objectId: "0000000000000000000000000000000000000000000000000000000000000001", pk: pk1.toBytes(), name: "test", url: "https://test.com", key_type: 0},
-	//       { objectId: "0000000000000000000000000000000000000000000000000000000000000002", pk: pk2.toBytes(), name: "test2", url: "https://test2.com", key_type: 0},
-	//       { objectId: "0000000000000000000000000000000000000000000000000000000000000003", pk: pk3.toBytes(), name: "test3", url: "https://test3.com", key_type: 0},
-	//     ],
-	//     2,
-	//     fromHEX("0000000000000000000000000000000000000000000000000000000000000000"),
-	//     fromHEX("01020304"),
-	//     new Plain(),
-	//   );
-	//
-	//   const parsed = EncryptedObject.parse(encryption);
-	//
-	//   const id = createFullId(DST, parsed.package_id, new Uint8Array(parsed.inner_id));
-	//
-	//   const usk1 = extractUserSecretKey(sk1, id);
-	//   const usk2 = extractUserSecretKey(sk2, id);
-	//   const usk3 = extractUserSecretKey(sk3, id);
-	//
-	//   const key = await decrypt(
-	//     parsed,
-	//     [usk1.toBytes(), usk2.toBytes(), usk3.toBytes()],
-	//   );
-	//
-	//   const modified_parsed_23 = parsed;
-	//   modified_parsed_23.services = parsed.services.slice(1);
-	//   modified_parsed_23.encrypted_shares.BonehFranklinBLS12381.shares = parsed.encrypted_shares.BonehFranklinBLS12381.shares.slice(1);
-	//   const key2 = await decrypt(
-	//     modified_parsed_23,
-	//     [usk2.toBytes(), usk3.toBytes()],
-	//   );
-	//
-	//   expect(key).toEqual(key2);
-	//
-	//   // Giving only one share will make the decryption fail.
-	//   const modified_parsed_1 = parsed;
-	//   modified_parsed_1.services = parsed.services.slice(0, 1);
-	//   modified_parsed_1.encrypted_shares.BonehFranklinBLS12381.shares = parsed.encrypted_shares.BonehFranklinBLS12381.shares.slice(0, 1);
-	//   await expect(
-	//     decrypt(
-	//       modified_parsed_1,
-	//       [usk2.toBytes()],
-	//     ),
-	//   ).rejects.toThrow();
-	//
-	//   // But if we're also decreasing the threshold on the EncryptedObject the decryption succeeds, but the key is not the correct one.
-	//   const modified_parsed_1_threshold = parsed;
-	//   modified_parsed_1_threshold.services = parsed.services.slice(0, 1);
-	//   modified_parsed_1_threshold.encrypted_shares.BonehFranklinBLS12381.shares = parsed.encrypted_shares.BonehFranklinBLS12381.shares.slice(0, 1);
-	//   modified_parsed_1_threshold.threshold = 1;
-	//   const key3 = await decrypt(
-	//       modified_parsed_1,
-	//       [usk2.toBytes()],
-	//     );
-	//   expect(key3).not.toEqual(key);
-	// });
-});
 
-describe('encryption scheme', () => {
-	it('hash regression test', async () => {
+	it('test encryption roundtrip with Plain-mode', async () => {
+		const [sk1, pk1] = generateKeyPair();
+		const [sk2, pk2] = generateKeyPair();
+		const [sk3, pk3] = generateKeyPair();
+
+		const objectId1 = fromHex('0000000000000000000000000000000000000000000000000000000000000001');
+		const objectId2 = fromHex('0000000000000000000000000000000000000000000000000000000000000002');
+		const objectId3 = fromHex('0000000000000000000000000000000000000000000000000000000000000003');
+
+		const encryption = await encrypt(
+			[
+				{
+					objectId: objectId1,
+					pk: pk1.toBytes(),
+					name: 'test',
+					url: 'https://test.com',
+					keyType: 0,
+				},
+				{
+					objectId: objectId2,
+					pk: pk2.toBytes(),
+					name: 'test2',
+					url: 'https://test2.com',
+					keyType: 0,
+				},
+				{
+					objectId: objectId3,
+					pk: pk3.toBytes(),
+					name: 'test3',
+					url: 'https://test3.com',
+					keyType: 0,
+				},
+			],
+			2,
+			fromHex('0000000000000000000000000000000000000000000000000000000000000000'),
+			fromHex('01020304'),
+			new Plain(),
+		);
+
+		const parsed = EncryptedObject.parse(encryption);
+
+		const id = createFullId(DST, parsed.package_id, new Uint8Array(parsed.inner_id));
+
+		const usk1 = extractUserSecretKey(sk1, id);
+		const usk2 = extractUserSecretKey(sk2, id);
+		const usk3 = extractUserSecretKey(sk3, id);
+		const key_store = new KeyStore();
+		key_store.addKey(toHex(id), objectId1, usk1.toBytes());
+		key_store.addKey(toHex(id), objectId2, usk2.toBytes());
+		key_store.addKey(toHex(id), objectId3, usk3.toBytes());
+
+		const key = await key_store.decrypt(parsed);
+
+		const key_store_23 = new KeyStore();
+		key_store_23.addKey(toHex(id), objectId2, usk2.toBytes());
+		key_store_23.addKey(toHex(id), objectId3, usk3.toBytes());
+		const modified_parsed_23 = parsed;
+		modified_parsed_23.services = parsed.services.slice(1);
+		modified_parsed_23.encrypted_shares.BonehFranklinBLS12381.shares =
+			parsed.encrypted_shares.BonehFranklinBLS12381.shares.slice(1);
+		await expect(key_store_23.decrypt(modified_parsed_23)).resolves.toEqual(key);
+
+		const key_store_1 = new KeyStore();
+		key_store_1.addKey(toHex(id), objectId1, usk1.toBytes());
+		const modified_parsed_1 = parsed;
+		modified_parsed_1.services = parsed.services.slice(0, 1);
+		modified_parsed_1.encrypted_shares.BonehFranklinBLS12381.shares =
+			parsed.encrypted_shares.BonehFranklinBLS12381.shares.slice(0, 1);
+		await expect(key_store_1.decrypt(modified_parsed_1)).rejects.toThrow();
+	});
+
+	it('G1 hash-to-curve regression test', async () => {
 		const package_id = new Uint8Array(32);
 		const inner_id = new Uint8Array([1, 2, 3, 4]);
 		const hash = G1Element.hashToCurve(createFullId(DST, package_id, inner_id));
@@ -234,10 +240,8 @@ describe('encryption scheme', () => {
 			'b32685b6ffd1f373faf3abb10c05772e033f75da8af729c3611d81aea845670db48ceadd0132d3a667dbbaa36acefac7';
 		expect(toHex(hash.toBytes())).toEqual(expected);
 	});
-});
 
-describe('kdf', () => {
-	it('regression test', () => {
+	it('kdf regression test', () => {
 		const x = G1Element.generator().pairing(
 			G2Element.generator().multiply(Scalar.fromNumber(12345)),
 		);
