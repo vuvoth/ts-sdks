df3466af5fed316185052425b27dfaeda4d4ae01
hayes-mysten <135670682+hayes-mysten@users.noreply.github.com>
2025-03-26 15:29:53 -0700

    Update fumadocs (#138)
    * bump seal version
    
    * update fumadocs

--- packages/docs/lib/source.ts
+++ packages/docs/lib/source.ts
@@ -1,11 +1,10 @@
 // Copyright (c) Mysten Labs, Inc.
 // SPDX-License-Identifier: Apache-2.0
 import { loader } from 'fumadocs-core/source';
-import { createMDXSource } from 'fumadocs-mdx';
 
-import { docs, meta } from '@/.source';
+import { docs } from '@/.source';
 
 export const source = loader({
 	baseUrl: '/',
-	source: createMDXSource(docs, meta),
+	source: docs.toFumadocsSource(),
 });
--- packages/docs/source.config.ts
+++ packages/docs/source.config.ts
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 import { defineConfig, defineDocs } from 'fumadocs-mdx/config';
 
-export const { docs, meta } = defineDocs({
+export const docs = defineDocs({
 	dir: 'content',
 });
 
