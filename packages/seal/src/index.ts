d899c1d213a58919c6e945cb727a1209e3bba7da
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2025-03-31 20:10:09 +0800

    export EncryptedObject (#154)

--- packages/seal/src/index.ts
+++ packages/seal/src/index.ts
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export { getAllowlistedKeyServers } from './key-server.js';
+export { EncryptedObject } from './bcs.js';
 export { SealClient, type SealClientOptions } from './client.js';
 export { SessionKey } from './session-key.js';
 export * from './error.js';
