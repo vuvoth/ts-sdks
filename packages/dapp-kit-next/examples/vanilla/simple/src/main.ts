822f59cb912a03fc5c6bfd3c8b4cb53bf92e222d
William Robertson <williamrobertson13@users.noreply.github.com>
2025-06-12 22:14:05 -0400

    [dapp-kit-next] add vanilla TS example (#374)

---
+++ packages/dapp-kit-next/examples/vanilla/simple/src/main.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Mysten Labs, Inc.
+// SPDX-License-Identifier: Apache-2.0
+
+import { createDAppKit } from '@mysten/dapp-kit-core';
+import { getFullnodeUrl, SuiClient } from '@mysten/sui/client';
+
+const connectButton = document.querySelector('mysten-dapp-kit-connect-button');
+connectButton!.instance = createDAppKit({
+	networks: ['mainnet', 'testnet'],
+	defaultNetwork: 'testnet',
+	createClient(network) {
+		return new SuiClient({ network, url: getFullnodeUrl(network) });
+	},
+});
