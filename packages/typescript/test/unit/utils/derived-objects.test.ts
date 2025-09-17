fe8b58b1114f16b330cedaf526d8c1a53bed83a1
Manolis Liolios <manos@mystenlabs.com>
2025-09-17 21:18:26 +0300

    [docs] Add derived object docs (#549)
    * Add derived object TS docs
    
    * Fix
    
    * remove normalize, nest under utils, fix/enhance tests to include the doc cases
    
    * remove from base meta

--- packages/typescript/test/unit/utils/derived-objects.test.ts
+++ packages/typescript/test/unit/utils/derived-objects.test.ts
@@ -5,7 +5,6 @@ import { describe, expect, test } from 'vitest';
 
 import { bcs, TypeTagSerializer } from '../../../src/bcs';
 import { deriveObjectID } from '../../../src/utils/derived-objects';
-import { normalizeSuiAddress } from '../../../src/utils';
 
 // Snapshots are recreated from `derived_object_tests.move` file,
 // as well as `sui-types/derived-object.rs` file.
@@ -13,12 +12,12 @@ describe('derived object test utils', () => {
 	test('deriveObjectID with primitive type', () => {
 		const key = bcs.vector(bcs.u8()).serialize(new TextEncoder().encode('foo')).toBytes();
 
-		expect(deriveObjectID(normalizeSuiAddress('0x2'), 'vector<u8>', key)).toBe(
+		expect(deriveObjectID('0x2', 'vector<u8>', key)).toBe(
 			'0xa2b411aa9588c398d8e3bc97dddbdd430b5ded7f81545d05e33916c3ca0f30c3',
 		);
-		expect(
-			deriveObjectID(normalizeSuiAddress('0x2'), TypeTagSerializer.parseFromStr('vector<u8>'), key),
-		).toBe('0xa2b411aa9588c398d8e3bc97dddbdd430b5ded7f81545d05e33916c3ca0f30c3');
+		expect(deriveObjectID('0x2', TypeTagSerializer.parseFromStr('vector<u8>'), key)).toBe(
+			'0xa2b411aa9588c398d8e3bc97dddbdd430b5ded7f81545d05e33916c3ca0f30c3',
+		);
 	});
 
 	test('deriveObjectID with struct type', () => {
@@ -66,5 +65,24 @@ describe('derived object test utils', () => {
 		expect(deriveObjectID('0x2', TypeTagSerializer.parseFromStr('u8'), key)).toBe(
 			'0x4de7696edddfb592a8dc7c8b66053b1557eb4fa1a9194322562aabf3da9e9239',
 		);
+	});
+
+	test('deriveObjectId from docs', () => {
+		const parentAddress = '0xc0ffee';
+
+		expect(deriveObjectID(parentAddress, 'address', bcs.Address.serialize('0x111').toBytes())).toBe(
+			'0x190ed830d9b453c5c620dc0385ced49cc84804644f5dbe66aa08c0e21947d1d4',
+		);
+		expect(
+			deriveObjectID(parentAddress, '0x1::string::String', bcs.String.serialize('foo').toBytes()),
+		).toBe('0x699219f4a2b6cfb8640bb853fc4ab4f497da038ec0614bfa2835aa27993399db');
+
+		expect(
+			deriveObjectID(
+				parentAddress,
+				'vector<u8>',
+				bcs.vector(bcs.u8()).serialize([0, 1, 2]).toBytes(),
+			),
+		).toBe('0x9067661aa45d09dac59d3eec926f8cfabb6e1ec57a9803ebc170723d3b19a9e2');
 	});
 });
